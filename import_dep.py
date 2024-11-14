1f79195eedc1e8903c9576924fb12c179fe0ac7e
hc3413 <horatiorjcox@gmail.com>
2024-11-14 10:33:44 +0100

    initializing project

---
+++ import_dep.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import datetime
+import pathlib
+from pathlib import Path
+import math
+import scipy
+from scipy.optimize import fsolve
+from scipy.stats import linregress
+from pptx import Presentation
+from pptx.util import Inches, Pt, Cm
+from pptx.dml.color import RGBColor
+from pptx.enum.text import PP_ALIGN
+from pptx.oxml.ns import qn
+from pptx.oxml import parse_xml
+
+
+
+
+
+
+
+
